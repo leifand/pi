bd6f2b00fbce22a5868ff8e7a605028a5d554525
Leif Anderson <leif.v.anderson@gmail.com>
2018-04-14 04:08:05 +0000

    adding example code

--- example1.py
+++ example1.py
@@ -1,11 +1,8 @@
-# worldbuilder.py
+# example1.py
 # 4/14/18
-# Dallas Young Makers
-#
-# basic edit operations for Minecraft Pi
-#
 from mcpi.minecraft import Minecraft
 from mcpi import block
+import time
 
 mc = Minecraft.create();
 
@@ -22,28 +19,29 @@ def cut_blocks(x0,y0,z0,x1,y1,z1):
     delete_blocks(x0,y0,z0,x1,y1,z1)
     return clipboard                
 
-def copy_blocks(x0,y0,z0,x1,y1,z1):    
-    clipboard = []
-    for i in range(abs(x1-x0)):
-        for j in range(abs(y1-y0)):
-            for k in range(abs(z1-z0)):
-                blk = mc.getBlock(i+x0,j+y0,k+z0)
-                clipboard.append([i,j,k,blk])
-    return clipboard
+#def copy_blocks(x0,y0,z0,x1,y1,z1):
+#    start_time = time.time()
+#
+# TODO: implement copy_blocks function
+#
+#    print (time.time() - start_time)
     
-def paste_blocks(x0,y0,z0,clipboard):
-    for i in range(len(clipboard)):
-        curr = clipboard[i]
-        mc.setBlock(curr[0]+x0,curr[1]+y0,curr[2]+z0,curr[3])
+#def paste_blocks(x0,y0,z0,clipboard):
+#
+# TODO: implement paste_blocks function
+#
 
 def save_blocks(clipboard,outfile):
+    start_time = time.time()
     f = open(outfile, "w")
     for i in range(len(clipboard)):
         curr = clipboard[i]    
         f.write(str(curr[0])+'\n'+str(curr[1])+'\n'+str(curr[2])+'\n'+str(curr[3])+'\n')
     f.close()
+    print (time.time() - start_time)    
 
 def read_blocks(infile):
+    start_time = time.time()
     f = open(infile, "r")
     clipboard = []
     curr = []
@@ -57,24 +55,10 @@ def read_blocks(infile):
         else:
             count+=1
     f.close()
+    print (time.time() - start_time)
     return clipboard
 
-def fix_block_file(infile,outfile):
-    f = open(infile, "r")
-    clipboard = []
-    curr = []
-    count = 0
-    for line in f:
-        curr.append(int(line))
-        if count == 3:
-            clipboard.append(curr)
-            count = 0
-            curr = []
-        else:
-            count+=1
-    f.close()
-    f = open(outfile, "w")
-    for i in range(len(clipboard)):
-        curr = clipboard[i]    
-        f.write(str(curr[1])+'\n'+str(curr[2])+'\n'+str(curr[3])+'\n'+str(curr[0])+'\n')
-    f.close()
+
+
+
+    
