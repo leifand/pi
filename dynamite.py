38dbd0aea8be77e9cc513fd65f1995b0b5ce2dd7
Leif Anderson <leif.v.anderson@gmail.com>
2018-11-23 16:32:49 +0000

    countdown timer

--- dynamite.py
+++ dynamite.py
@@ -6,20 +6,27 @@ import time
 from time import sleep
 
 mc = Minecraft.create()
+tnt = 46
 
 def set_dynamite():
-    pass
+    x,y,z = mc.player.getPos()
+    mc.setBlock(x,y,z,tnt,1)
 
-def set_dynamite_block(x,y,z):
-    pass
+def set_dynamite_blocks(x0,y0,z0,x1,y1,z1):
+    mc.setBlocks(x0,y0,z0,x1,y1,z1,tnt,1)
 
 def dynamite_trail(seconds):
-    tnt = 46
+    for t in range(5):
+        mc.postToChat(5-t)
+        sleep(1.0)
     start_time = time.time()
     while time.time() - start_time < seconds:
         x,y,z = mc.player.getPos()
         mc.setBlock(x,y,z,tnt,1)
         sleep(0.1)
     mc.postToChat("GAME OVER")
+
+def dynamite_grid(rows,cols):
+    pass
 
 mc.postToChat("The dynamite library has been loaded ...")
